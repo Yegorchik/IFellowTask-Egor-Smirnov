3463bacc91e4753f8e9b1dd709f1aa2e47a26812
Yegor <carrrrpet@gmail.com>
2023-12-12 19:14:54 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Выкачать с гита
+2. Cобрать проект maven
+3. Запустить в терминале(windows) с помощью: java -cp IFellowTask-Egor-Smirnov-1.0-jar-with-dependencies.jar com.ifellowtask.Main "путь к файлу для сортировки"
--- src/main/java/com/ifellowtask/FileSorter.java
+++ src/main/java/com/ifellowtask/FileSorter.java
@@ -14,6 +14,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
+
 public class FileSorter {
     private static final long memory = (long) (Runtime.getRuntime().freeMemory() * 0.8);
 
